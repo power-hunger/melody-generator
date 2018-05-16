d4c15cb539a8004afd53beddb00684ec858bcf70
Konrads <konrads.buss@gmail.com>
2018-05-16 16:30:29 +0300

    midi file parsing added main func

--- prepareDataset.py
+++ prepareDataset.py
@@ -176,14 +176,11 @@ string_instruments = ["StringInstrument", "Violin", "Viola", "Violoncello", "Con
 dir_path = "/Users/konradsbuss/Documents/Uni/bak/dataset/lmd_full/e"
 output_file_path = "/Users/konradsbuss/Documents/Uni/bak/dataset/preparedData/filename_full.txt"
 output_file_path_same_note_length01 = "/Users/konradsbuss/Documents/Uni/bak/dataset/preparedData/filename_same_note_len_01.txt"
-output_file_path_same_note_length005 = "/Users/konradsbuss/Documents/Uni/bak/dataset/preparedData/filename_same_note_len_005.txt"
 output_file_path_same_note_exception = "/Users/konradsbuss/Documents/Uni/bak/dataset/preparedData/filename_exception.txt"
-output_file_path_percentage = "/Users/konradsbuss/Documents/Uni/bak/dataset/preparedData/filename_percentage.txt"
 output_file_path_csv = "/Users/konradsbuss/Documents/Uni/bak/dataset/preparedData/filename_csv.csv"
 
 
 def prepare_data():
-    init_csv_file(output_file_path_csv)
     path_list = []
     for midi_file_path in Path(dir_path).glob('**/*.mid'):
         path_list.append(midi_file_path)
@@ -278,4 +275,6 @@ def init_csv_file(csv_file_path):
         writer.writeheader()
 
 
-prepare_data()
+if __name__ == '__main__':
+    init_csv_file(output_file_path_csv)
+    prepare_data()
